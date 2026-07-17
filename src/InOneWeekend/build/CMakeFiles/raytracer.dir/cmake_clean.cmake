6b6dde9459056a6a9e38efc14ae528b6fef61751
bmonagan <bernardmonagan@gmail.com>
2026-07-17 03:57:32 -0400

    cmake setup for minimalist raytracing

---
+++ src/InOneWeekend/build/CMakeFiles/raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer.dir/link.d"
+  "CMakeFiles/raytracer.dir/src/main.cpp.o"
+  "CMakeFiles/raytracer.dir/src/main.cpp.o.d"
+  "raytracer"
+  "raytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
