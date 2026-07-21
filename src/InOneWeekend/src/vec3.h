d968a221af4271e78d6dd4ebddca776784eb543f
bmonagan <bernardmonagan@gmail.com>
2026-07-20 21:47:58 -0400

    fixed more erros that were in the vec3 file

--- src/InOneWeekend/src/vec3.h
+++ src/InOneWeekend/src/vec3.h
@@ -8,13 +8,13 @@ class vec3 {
   public:
     double e[3];
     vec3() : e(0,0,0) {}
-    vec3(double eo, double e1, double e2) : e(e0, e1 ,e2) {}
+    vec3(double e0, double e1, double e2) : e(e0, e1 ,e2) {}
 
     double x() const { return e[0]; }
     double y() const { return e[1]; }
     double z() const { return e[2]; }
 
-    vec3 operator() const { return vec3(-e[0], -e[1], -e[2]); }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) { return e[i]; }
 
@@ -54,15 +54,15 @@ inline std::ostream& operator <<(std::ostream& out, const vec3& v) {
 }
 
 inline vec3 operator+(const vec3& u, const vec3& v) {
-  return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2])
+  return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
 inline vec3 operator-(const vec3& u, const vec3& v) {
-  return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2])
+  return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 inline vec3 operator*(const vec3& u, const vec3& v) {
-  return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2])
+  return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 inline vec3 operator*(double t, const vec3& v) {
