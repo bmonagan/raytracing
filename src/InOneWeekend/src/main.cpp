40c9883d7ecede5fc8e4d408a4cf2069cc6a7034
bmonagan <bernardmonagan@gmail.com>
2026-07-20 21:35:23 -0400

    include the color and vec header files into main and use them to show color.

--- src/InOneWeekend/src/main.cpp
+++ src/InOneWeekend/src/main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -15,15 +18,8 @@ int main() {
   for (int j{}; j < image_height; j++) {
     std::clog << "\rScanLines remaining: " << (image_height-j) << ' ' << std::flush;
     for (int i{}; i < image_width; i++) {
-      auto r = double(i) / (image_width-1);
-      auto g = double(j) / (image_height-1);
-      auto b = 0.0;
-
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_width-1), 0);
+      write_color(std::cout, pixel_color);
     }
   }
 
