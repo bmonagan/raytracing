790a28244ac40adc552692ed767aa0c25b099751
bmonagan <bernardmonagan@gmail.com>
2026-07-27 19:13:13 -0400

    adds an implementation for a random double.

--- src/InOneWeekend/src/rtweekend.h
+++ src/InOneWeekend/src/rtweekend.h
@@ -3,6 +3,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -20,6 +21,15 @@ inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+  // Returns a random real in [0,1)).
+  return std::rang() / (RAND_MAX + 1.0);
+}
+
+inline double random_double (double min, double max) {
+  // Returns a random real in [min,max)
+  return min + (max-min)*random_double();
+}
 // Common Headers
 
 #include "color.h"
