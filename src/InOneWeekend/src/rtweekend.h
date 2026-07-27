9a15bd29dcb05f1fb1b8061c4dd187121c1153e5
bmonagan <bernardmonagan@gmail.com>
2026-07-27 19:37:57 -0400

    fixed a spelling error in rand

--- src/InOneWeekend/src/rtweekend.h
+++ src/InOneWeekend/src/rtweekend.h
@@ -1,6 +1,7 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
+#include <random>
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
@@ -23,7 +24,7 @@ inline double degrees_to_radians(double degrees) {
 
 inline double random_double() {
   // Returns a random real in [0,1)).
-  return std::rang() / (RAND_MAX + 1.0);
+  return std::rand() / (RAND_MAX + 1.0);
 }
 
 inline double random_double (double min, double max) {
