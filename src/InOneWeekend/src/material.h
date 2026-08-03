5f16d23abbd9f9c9e486541ab8f413d61d99b0ec
bmonagan <bernardmonagan@gmail.com>
2026-08-03 16:56:16 -0400

    added schlicks approximation to dielectric

--- src/InOneWeekend/src/material.h
+++ src/InOneWeekend/src/material.h
@@ -67,7 +67,7 @@ class dielectric : public material {
 
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
-        if (cannot_refract) 
+        if (cannot_refract || reflectance(cost_theta, ri) > random_double()) 
           direction = reflect(unit_direction, rec.normal);
         else {
           direction = refract(unit_direction, rec.normal, ri);
@@ -79,6 +79,13 @@ class dielectric : public material {
     // Refractive index in vacuum or air, or the ratio of the material's refactive index override
     // the refractive index of the enclosing media
     double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) {
+      // Use Schlicks approximation for reflectance
+      auto r0 = (1 - refraction_index) / (1 + refraction_index);
+      r0 = r0*r0;
+      return r0 + (r-r0)*std::pow((1 - cosine), 5);
+    }
 };
 #endif
 
