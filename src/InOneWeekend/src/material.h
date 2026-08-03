8a43df73009e52d93e264d5f6e94a11eb3ca2082
bmonagan <bernardmonagan@gmail.com>
2026-08-03 17:00:19 -0400

    fixed errors in dielectric class

--- src/InOneWeekend/src/material.h
+++ src/InOneWeekend/src/material.h
@@ -67,7 +67,7 @@ class dielectric : public material {
 
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
-        if (cannot_refract || reflectance(cost_theta, ri) > random_double()) 
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double()) 
           direction = reflect(unit_direction, rec.normal);
         else {
           direction = refract(unit_direction, rec.normal, ri);
@@ -84,7 +84,7 @@ class dielectric : public material {
       // Use Schlicks approximation for reflectance
       auto r0 = (1 - refraction_index) / (1 + refraction_index);
       r0 = r0*r0;
-      return r0 + (r-r0)*std::pow((1 - cosine), 5);
+      return r0 + (1-r0)*std::pow((1 - cosine), 5);
     }
 };
 #endif
