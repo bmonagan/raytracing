e1905391a74ee829964dfa802faa349a35e0c552
bmonagan <bernardmonagan@gmail.com>
2026-08-03 21:04:20 -0400

    added true to make the while look correct

--- src/InOneWeekend/src/vec3.h
+++ src/InOneWeekend/src/vec3.h
@@ -128,7 +128,7 @@ inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
 }
 
 inline vec3 random_in_unit_disk() {
-  while () {
+  while (true) {
     auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
     if (p.length_squared() < 1)
       return p;
