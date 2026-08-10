917d5b619c9950242651324647716a8101c38d94
bmonagan <bernardmonagan@gmail.com>
2026-08-10 19:28:54 -0400

    changed the hit function to work with moving spheres

--- src/InOneWeekend/src/sphere.h
+++ src/InOneWeekend/src/sphere.h
@@ -17,7 +17,8 @@ public:
         mat(mat) {}
 
   bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
-    vec3 oc = center - r.origin();
+    point3 current_center = center.at(r.time());
+    vec3 oc = current_center - r.origin();
     auto a = r.direction().length_squared();
     auto h = dot(r.direction(), oc);
     auto c = oc.length_squared() - radius * radius;
@@ -38,7 +39,7 @@ public:
 
     rec.t = root;
     rec.p = r.at(rec.t);
-    vec3 outward_normal = (rec.p - center) / radius;
+    vec3 outward_normal = (rec.p - current_center) / radius;
     rec.set_face_normal(r, outward_normal);
     rec.mat = mat;
 
