3646f5a9156891e35b06d50ac91e02112a56ddbd
bmonagan <bernardmonagan@gmail.com>
2026-08-10 21:48:29 -0400

    add expand to interval class

--- src/InOneWeekend/src/interval.h
+++ src/InOneWeekend/src/interval.h
@@ -2,36 +2,35 @@
 #define INTERVAL_H
 
 class interval {
-  public:
-    double min, max;
+public:
+  double min, max;
 
-    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+  interval() : min(+infinity), max(-infinity) {} // Default interval is empty
 
-    interval(double min, double max) : min(min), max(max) {}
+  interval(double min, double max) : min(min), max(max) {}
 
-    double size() const {
-      return max - min;
-    }
+  double size() const { return max - min; }
 
-    bool contains(double x) const {
-      return min <= x && x <= max;
-    }
+  bool contains(double x) const { return min <= x && x <= max; }
 
-    bool surrounds(double x) const {
+  bool surrounds(double x) const { return min < x && x < max; }
 
-      return min < x && x < max;
-    }
+  double clamp(double x) const {
+    if (x < min)
+      return min;
+    if (x > max)
+      return max;
+    return x;
+  }
+  interval expand(double delta) const {
+    auto padding = delta / 2;
+    return interval(min - padding, max + padding);
+  }
 
-    double clamp(double x) const {
-      if (x < min) return min;
-      if (x > max) return max;
-      return x;
-    }
-
-    static const interval empty, universe;
+  static const interval empty, universe;
 };
 
-const interval interval::empty    = interval(+infinity, -infinity);
+const interval interval::empty = interval(+infinity, -infinity);
 const interval interval::universe = interval(-infinity, +infinity);
 
 #endif
