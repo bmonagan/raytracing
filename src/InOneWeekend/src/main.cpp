47986908c53e6c38995a2249ff4416eb4554a322
bmonagan <bernardmonagan@gmail.com>
2026-08-17 15:51:12 -0400

    add the earth function and create a switch choice for it

--- src/InOneWeekend/src/main.cpp
+++ src/InOneWeekend/src/main.cpp
@@ -9,6 +9,28 @@
 #include "texture.h"
 #include <memory>
 
+void earth() {
+  auto earth_texture = std::make_shared<image_texture>("earthmap.jpg");
+  auto earth_surface = make_shared<lambertian>(earth_texture);
+  auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+  camera cam;
+
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
+  cam.samples_per_pixel = 100;
+  cam.max_depth = 50;
+
+  cam.vfov = 20;
+  cam.lookfrom = point3(0, 0, 12);
+  cam.lookat = point3(0, 0, 0);
+  cam.vup = vec3(0, 1, 0);
+
+  cam.defocus_angle = 0;
+
+  cam.render(hittable_list(globe));
+}
+
 void bouncing_spheres() {
   hittable_list world;
 
@@ -104,12 +126,15 @@ void checkered_spheres() {
 }
 
 int main() {
-  switch (2) {
+  switch (3) {
   case 1:
     bouncing_spheres();
     break;
   case 2:
     checkered_spheres();
+    break;
+  case 3:
+    earth();
     break;
   }
 }
