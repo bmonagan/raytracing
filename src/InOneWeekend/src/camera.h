7bff4ce77ead31a3285f783687e1da19164c758b
bmonagan <bernardmonagan@gmail.com>
2026-08-24 16:24:37 -0400

    removed hanging return

--- src/InOneWeekend/src/camera.h
+++ src/InOneWeekend/src/camera.h
@@ -143,12 +143,6 @@ private:
 
     return color_from_emission + color_from_scatter;
   }
-
-  vec3 unit_direction = unit_vector(r.direction());
-  auto a = 0.5 * (unit_direction.y() + 1.0);
-  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
-}
-;
+};
 
 #endif
