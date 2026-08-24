5d994ea0281abfead3f41e0377474ccff3a69ac8
bmonagan <bernardmonagan@gmail.com>
2026-08-24 18:30:23 -0400

    added more to the translate class

--- src/InOneWeekend/src/hittable.h
+++ src/InOneWeekend/src/hittable.h
@@ -35,6 +35,10 @@ public:
 
 class traslate : public hittable {
 public:
+  translate(shared_ptr<hittable> object, const vec3 &offset)
+      : object(object), offset(offset) {
+    bbox = object->bounding_box() + offset;
+  }
   bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
     // Move the ray backwards by the offset
     ray offset_r(r.origin() - offset, r.direction(), r.time());
@@ -50,9 +54,12 @@ public:
     return true;
   }
 
+  aabb bounding_box() const override { return bbox; }
+
 private:
   shared_ptr<hittable> object;
   vec3 offset;
+  aabb bbox;
 };
 
 #endif
