825b060b0b2b43025f81452697b0efe16239c274
bmonagan <bernardmonagan@gmail.com>
2026-08-24 18:35:50 -0400

    add rotate y hittable class

--- src/InOneWeekend/src/hittable.h
+++ src/InOneWeekend/src/hittable.h
@@ -62,4 +62,41 @@ private:
   aabb bbox;
 };
 
+class rotate_y : public hittable {
+public:
+  bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
+
+    // Transform the ray from world space to object space.
+
+    auto origin =
+        point3((cos_theta * r.origin().x()) - (sin_theta * r.origin().z()),
+               r.origin().y(),
+               (sin_theta * r.origin().x()) + (cos_theta * r.origin().z()));
+
+    auto direction =
+        vec3((cos_theta * r.direction().x()) - (sin_theta * r.direction().z()),
+             r.direction().y(),
+             (sin_theta * r.direction().x()) + (cos_theta * r.direction().z()));
+
+    ray rotated_r(origin, direction, r.time());
+
+    // Determine whether an intersection exists in object space (and if so,
+    // where).
+
+    if (!object->hit(rotated_r, ray_t, rec))
+      return false;
+
+    // Transform the intersection from object space back to world space.
+
+    rec.p = point3((cos_theta * rec.p.x()) + (sin_theta * rec.p.z()), rec.p.y(),
+                   (-sin_theta * rec.p.x()) + (cos_theta * rec.p.z()));
+
+    rec.normal =
+        vec3((cos_theta * rec.normal.x()) + (sin_theta * rec.normal.z()),
+             rec.normal.y(),
+             (-sin_theta * rec.normal.x()) + (cos_theta * rec.normal.z()));
+
+    return true;
+  }
+};
 #endif
