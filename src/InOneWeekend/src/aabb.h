08f01e061b656ff9351cfeaf4480f75996277ef3
bmonagan <bernardmonagan@gmail.com>
2026-08-24 15:09:59 -0400

    add pad to minumum method to aabb

--- src/InOneWeekend/src/aabb.h
+++ src/InOneWeekend/src/aabb.h
@@ -8,7 +8,9 @@ public:
   aabb() {}
 
   aabb(const interval &x, const interval &y, const interval &z)
-      : x(x), y(y), z(z) {}
+      : x(x), y(y), z(z) {
+    pad_to_minimum();
+  }
 
   aabb(const point3 &a, const point3 &b) {
     // Treat the two points and b as extrema for the bounding box, so we dont
@@ -17,6 +19,8 @@ public:
     x = (a[0] <= b[0]) ? interval(a[0], b[0]) : interval(b[0], a[0]);
     y = (a[1] <= b[1]) ? interval(a[1], b[1]) : interval(b[1], a[1]);
     z = (a[2] <= b[2]) ? interval(a[2], b[2]) : interval(b[2], a[2]);
+
+    pad_to_minimum();
   }
 
   aabb(const aabb &box0, const aabb &box1) {
@@ -69,6 +73,20 @@ public:
       return y.size() > z.size() ? 1 : 2;
   }
   static const aabb empty, universe;
+
+private:
+  void pad_to_minimum() {
+    // Adjust the AABB so that no side is narrower than some delta, padding if
+    // necessary
+
+    double delta = 0.0001;
+    if (x.size() < delta)
+      x = x.expand(delta);
+    if (y.size() < delta)
+      y = y.expand(delta);
+    if (z.size() < delta)
+      z = z.expand(delta);
+  }
 };
 
 const aabb aabb::empty =
